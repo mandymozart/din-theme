c768cbcfb8e397addc58c2825347675b05a91b58
porschuetz <tilman@sinnfrei.co.uk>
2014-09-15 19:36:39 +0200

    removed admin bar from frontend

--- functions.php
+++ functions.php
@@ -128,3 +128,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Remove Admin Bar from Frontend
+ */
+add_filter('show_admin_bar', '__return_false');
