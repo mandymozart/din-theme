b8490ead34ae78d74b6d68e1651bbab31d3bcc04
porschuetz <tilman@sinnfrei.co.uk>
2014-09-16 14:13:04 +0200

    offcanvas JS updated

--- din-theme/footer.php
+++ din-theme/footer.php
@@ -59,7 +59,8 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/javascripts/bootstrap.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.offcanvas.js"></script>
 
 </body>
 </html>
