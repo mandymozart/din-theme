c960832669cf05577d7df970c20fb2e88db52741
sb-services <sugabam@shalomsalon.de>
2014-10-01 20:37:20 +0200

    CHECK IT!!!!
    fonts.css nicht mergen
    rest siehste ja

--- page_forms-page.php
+++ page_forms-page.php
@@ -12,35 +12,45 @@
  */
 
 get_header(); ?>
-<section class="container">
+<section class="container section">
+    <!--div id="primary" class="content-area"-->
+    <!--main id="main" class="site-main" role="main"-->
+    <!--div id="forms-section" class="row-fluid"-->
 
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <?php get_template_part( 'content', 'page' ); ?>
+
+        <?php
+        // If comments are open or we have at least one comment, load up the comment template
+        if ( comments_open() || '0' != get_comments_number() ) :
+            comments_template();
+        endif;
+        ?>
+
+    <?php endwhile; // end of the loop. ?>
+
+    <!--/div--><!-- #forms section -->
+    <!--/main--><!-- #main -->
+    <!--/div--><!-- #primary -->
 </section>
 
-<section class="container-fluid">
-    <section class="row-fluid">
-        <section class=".col-md-6 .col-md-offset-3">
-                <!--div id="primary" class="content-area"-->
-                    <!--main id="main" class="site-main" role="main"-->
-                        <!--div id="forms-section" class="row-fluid"-->
+<section class="container-fluid section section-inverse">
+    <div class="container">
+        <div class="">
+          <h2>Formular für Gutachten</h2>
+            <p>Hier was mit dem von gut als mit eintragen wovon Licht viel besser.</p>
+        </div>
+    </div>
 
-                        <?php while ( have_posts() ) : the_post(); ?>
+    <div class="row-fluid">
+        <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+            <?php
+            if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 3 ); }
 
-                            <?php get_template_part( 'content', 'page' ); ?>
-
-                            <?php
-                                // If comments are open or we have at least one comment, load up the comment template
-                                if ( comments_open() || '0' != get_comments_number() ) :
-                                    comments_template();
-                                endif;
-                            ?>
-
-                        <?php endwhile; // end of the loop. ?>
-
-                        <!--/div--><!-- #forms section -->
-                    <!--/main--><!-- #main -->
-                <!--/div--><!-- #primary -->
-        </section>
-    </section>
+            ?>
+        </div>
+    </div>
 
 </section>
 
